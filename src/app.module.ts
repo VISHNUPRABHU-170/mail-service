47e4a11866742625fc1b52999693fbd9078d1c29
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2025-02-08 00:16:57 +0530

    Implemented Mail Service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthMiddleware } from './middleware/auth.middleware';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Makes the ConfigModule available globally
-    })
+    }),
+    MailerModule
   ],
 })
 export class AppModule {
